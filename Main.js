7ef47d4f8e802b3eb6748bb430330ea9deda6299
robofancz <123932205+robofancz@users.noreply.github.com>
2024-12-19 13:45:19 +0100

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,15 @@
+function photo() {
+    window.location.href = 'Photography.html';
+}
+function home(){
+    window.location.href='main.html'
+}
+function pro(){
+    window.location.href='Programming.html'
+}
+function contacts(){
+    window.location.href='Contacts.html'
+}
+function instagram(){
+    window.HTMLLinkElement.href='www.instagram.com'
+}
